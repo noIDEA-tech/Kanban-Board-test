43eeb4bd7d036858078b9aaa55cadf33b2d4c3c7
Nancy Watreas <nwatreas2023@gmail.com>
2025-02-24 16:47:26 -0600

    refactor: simplify TypeScript configuration and update build scripts, prepare for re-deploy

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     port: 3000,
@@ -19,3 +18,5 @@ export default defineConfig({
     },
   },
 });
+
+// https://vitejs.dev/config/
