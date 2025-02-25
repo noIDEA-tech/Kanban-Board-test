f5016171ec82794625027a0d0dd9000009058486
Nancy Watreas <nwatreas2023@gmail.com>
2025-02-25 10:36:52 -0600

    refactor: update server.ts to improve path handling and organize imports

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,20 @@
-const forceDatabaseRefresh = false;
-
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import path from 'path';
 import dotenv from 'dotenv';
-dotenv.config();
-
 import express from 'express';
 import routes from './routes/index.js';
 import { sequelize } from './models/index.js';
-import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const forceDatabaseRefresh = false;
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Serves static files in the entire client's dist folder
 app.use(express.static('../client/dist'));
 
 app.use(express.json());
